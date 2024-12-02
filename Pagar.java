2694a5476996e10b05c22002842c7d55bdcb9c5e
OctavioMonreal <omonreal@udesa.edu.ar>
2024-12-01 23:44:04 -0300

    Delete Pagar.java

--- Pagar.java
+++
@@ -1,9 +0,0 @@
-package GameNT;
-
-public class Pagar implements Accion {
-    public void ejecutar(NoThanks juego) {
-        juego.jugadores.get(juego.jugadorTurno).pagarFicha();
-        juego.fichasSobreCarta++;
-        juego.avanzarTurno();
-    }
-}

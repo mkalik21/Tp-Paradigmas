918c8a524218466762a657abefd019d8aa0d379c
Milena Kalik <110475642+mkalik21@users.noreply.github.com>
2024-12-01 23:24:12 -0300

    Update Comprar.java

--- Comprar.java
+++ Comprar.java
@@ -2,9 +2,10 @@ package GameNT;
 
 public class Comprar implements Accion {
     public void ejecutar(NoThanks juego) {
-        Card card = juego.deck.poll(); // Get the Card object from the deck
-        juego.jugadores.get(juego.jugadorTurno).tomarCarta(card); // Pass the Card object
+        Card card = juego.deck.iterator().next();
+        juego.jugadores.get(juego.jugadorTurno).tomarCarta(card);
         juego.jugadores.get(juego.jugadorTurno).recibirFichas(juego.fichasSobreCarta);
         juego.fichasSobreCarta = 0;
+        juego.deck.remove(card);
     }
 }
